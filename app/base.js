00ec5316fd5aa03612bd52360d2f5240679a5fca
Pete Blois <github@blois.us>
2015-11-23 16:28:39 -0800

    Adding missing manifest permission (activeTab).

--- app/base.js
+++ app/base.js
@@ -1,31 +1,5 @@
 var rooler = rooler || {};
 
-// From Mozilla's Dev Documentation.
-if (!Function.prototype.bind) {
-  Function.prototype.bind = function (oThis) {
-    if (typeof this !== "function") {
-      // closest thing possible to the ECMAScript 5 internal IsCallable function
-      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-    }
-
-    var fSlice = Array.prototype.slice,
-        aArgs = fSlice.call(arguments, 1),
-        fToBind = this,
-        fNOP = function () {},
-        fBound = function () {
-          return fToBind.apply(this instanceof fNOP
-                                 ? this
-                                 : oThis || window,
-                               aArgs.concat(fSlice.call(arguments)));
-        };
-
-    fNOP.prototype = this.prototype;
-    fBound.prototype = new fNOP();
-
-    return fBound;
-  };
-}
-
 rooler.clamp = function(value, min, max) {
   return Math.max(Math.min(value, max), min);
 }
